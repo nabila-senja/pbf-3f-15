a1a0b50232492d0590728b408291d8786d3c132f
nsenja <nabilasw34@gmail.com>
2022-03-14 10:51:17 +0700

    create blogpost in container

---
+++ week4-app/src/container/BlogPost/BlogPost.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import './BlogPost.css';
+import Post from "../../component/BlogPost/Post";
+
+class BlogPost extends Component {
+    render() {
+        return (
+            <div class="post-artikel">
+                <h2>Daftar Artikel</h2>
+                <Post judul="JTI Polinema" isi="Jurusan Teknologi Informasi-Politeknik Negeri Malang"/>
+                    </div>
+        )
+    }
+}
+export default BlogPost;
