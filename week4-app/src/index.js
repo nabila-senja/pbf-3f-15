c742826639863f36700d229c55b1c179ba3bd1a5
nsenja <nabilasw34@gmail.com>
2022-03-14 10:51:56 +0700

    praktikum muat data dinamis

--- week4-app/src/index.js
+++ week4-app/src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import BlogPost from './container/BlogPost/BlogPost';
+import 'bootstrap/dist/css/bootstrap.min.css';
+ReactDOM.render(<BlogPost/>, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
