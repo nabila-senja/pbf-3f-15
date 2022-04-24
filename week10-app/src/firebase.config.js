490f662eb65e3fc8d19876923981a18e81f7307b
nsenja <nabilasw34@gmail.com>
2022-04-24 19:21:14 +0700

    Praktikum Modul 9 - Firebase React

---
+++ week10-app/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyCo_YjaDnoHZtkoZiOtBe3_bbwFw_uPY1g",
+    authDomain: "week10-app.firebaseapp.com",
+    projectId: "week10-app",
+    storageBucket: "week10-app.appspot.com",
+    messagingSenderId: "257501016412",
+    appId: "1:257501016412:web:70ca414059d7ed04541199",
+    measurementId: "G-4K72K6TTH5"
+};
